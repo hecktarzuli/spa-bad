04088c94a0c87b64c25e060b5da5ef4710a07a7c
Josh Deltener <josh.deltener@realtruck.com>
2020-11-27 22:42:58 -0600

    init

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'spa-bad',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+}
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.title + ' | Social Sharing Test',
+      link: [{ rel: 'canonical', href: 'https://blarg.net' }],
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: "This is a description"
+        },
+        { hid: 'og-type', property: 'og:type', content: 'website' },
+        { hid: 'og-title', property: 'og:title', content: "Social Share Test" },
+        { hid: 'og-image', property: 'og:image', content: "https://images.unsplash.com/photo-1518331647614-7a1f04cd34cf?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2249&q=80" },
+        { hid: 'og-url', property: 'og:url', content: "https://blarg.net" },
+        {
+          hid: 'og-desc',
+          property: 'og:description',
+          content: "This is a description"
+        },
+        { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
+        { hid: 't-site', name: 'twitter:site', content: '@joshdeltener' }
+      ]
+    }
+  },
+}
+</script>
